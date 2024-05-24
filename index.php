ccf6ec70a2273b9890bba26478a921ef169ab0dd
jkagullo <jhankyle.agullo@tup.edu.ph>
2024-05-25 03:39:29 +0800

    changes

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="shortcut icon" href="assets/logo1.svg" type="image/x-icon">
     <link rel="stylesheet" href="index.css">
     <title>edvance.</title>
 </head>
@@ -17,14 +18,14 @@
     <form action="">
         <img src="assets/logo1.svg" alt="logo" class="logo">
         <h1>edvance</h1>
-        <h2>Welcome Back!</h2>
+        <h2>Welcome back!</h2>
     <div class="input-box">
         <i class='bx bxs-user'></i>
-        <input type="text" id="username" name="username" placeholder="username" required>
+        <input type="text" id="username" name="username" placeholder="Username" required>
     </div>
     <div class="input-box">
         <i class='bx bxs-lock-alt'></i>
-        <input type="password" id="password" name="password" placeholder="password" required>
+        <input type="password" id="password" name="password" placeholder="Password" required>
     </div>
       <button type="submit" class="btn">Login</button>
     </form>
---
+++ student-dashboard.php
@@ -0,0 +1,35 @@
+<?php
+    include "db.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="student-dashboard.css">
+    <title>Student Dashboard</title>
+</head>
+<body>
+    <nav class="sidebar">
+        <header>
+            <div class="image-text">
+                <span class="image">
+                    <img src="assets/logo-dark.svg" alt="logo">
+                </span>
+
+                <div class="text header-text">
+                    <span class="name">edvance</span>
+                    <span class="profession">Student</span>
+                </div>
+            </div>
+
+            <i class='bx bx-chevron-right toggle'></i>
+        </header>
+    </nav>
+</body>
+</html>
