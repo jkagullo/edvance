9b4d45b4d6bd593112d27577442e6b04f202b42a
jkagullo <jhankyle.agullo@tup.edu.ph>
2024-05-25 17:12:48 +0800

    created a new database

--- templates/student-dashboard.php
+++ templates/student-dashboard.php
@@ -7,6 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
@@ -23,13 +24,35 @@
                 </span>
 
                 <div class="text header-text">
-                    <span class="name">edvance</span>
+                    <span class="name">Edvance</span>
                     <span class="profession">Student</span>
                 </div>
             </div>
 
             <i class='bx bx-chevron-right toggle'></i>
         </header>
+        <div class="menu-bar">
+            <div class="menu">
+                <li class="search-box">
+                    <i class='bx bx-search icon' ></i>
+                    <input type="search" placeholder="Search">
+                </li>
+                <ul class="menu-links">
+                    <li class="nav-link">
+                        <a href="">
+                            <i class='bx bx-home-alt icon'></i>
+                            <span class="text nav-text">Dashboard</span>
+                        </a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="">
+                            <i class='bx bx-server icon' ></i>
+                            <span class="text nav-text">Grades</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </nav>
 </body>
 </html>
