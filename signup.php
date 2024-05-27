03fb4be5faf9c22fa086f66452c53e5fdb0f3a4b
jkagullo <jhankyle.agullo@tup.edu.ph>
2024-05-27 23:45:40 +0800

    signup done

--- signup.php
+++ signup.php
@@ -1,4 +1,34 @@
-<?php include "db.php"; ?>
+<?php 
+include "db.php"; 
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Get the submitted form data
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+    // Hash the password
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    
+    // Set the default role as 'student'
+    $role = 'student';
+
+    // Prepare and execute the SQL query to insert the new user
+    $stmt = $conn->prepare("INSERT INTO users (username, password, role) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $username, $hashed_password, $role);
+
+    if ($stmt->execute()) {
+        // Redirect to login page after successful signup
+        header("Location: index.php");
+        exit();
+    } else {
+        $error_message = "Error: " . $stmt->error;
+    }
+    
+    $stmt->close();
+}
+
+$conn->close();
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -16,37 +46,28 @@
 </head>
 <body>
   <div class="wrapper">
-    <form action="">
+    <form action="signup.php" method="POST">
         <img src="assets/logo1.svg" alt="logo" class="logo">
         <h2>Create an edvance account</h2>
         <p class="no-acc">Already have an account? <a href="index.php">Log in</a></p>
-    
-    <p class="label">First Name</p>
-    <div class="input-box">
-        <input type="text" id="username" name="username" placeholder="Enter your first name" required>
-    </div>
 
-    <p class="label">Last Name</p>
-    <div class="input-box">
-        <input type="text" id="username" name="username" placeholder="Enter your last name" required>
-    </div>
+        <?php if (isset($error_message)): ?>
+            <p class="error-message"><?php echo $error_message; ?></p>
+        <?php endif; ?>
 
-    <p class="label">Email</p>
-    <div class="input-box">
-        <input type="text" id="username" name="username" placeholder="your_name@gmail.com" required>
-    </div>
-
-    <div class="input-box">
-        <i class='bx bxs-user'></i>
-        <input type="text" id="username" name="username" placeholder="Username" required>
-    </div>
-    <div class="input-box">
-        <i class='bx bxs-lock-alt'></i>
-        <input type="password" id="password" name="password" placeholder="Password" required>
-    </div>
-      <button type="submit" class="btn">Signup</button>
+        <p class="label">Account Username</p>
+        <div class="input-box">
+            <i class='bx bxs-user'></i>
+            <input type="text" id="username" name="username" placeholder="Username" required>
+        </div>
+        
+        <p class="label">Account Password</p>
+        <div class="input-box">
+            <i class='bx bxs-lock-alt'></i>
+            <input type="password" id="password" name="password" placeholder="Password" required>
+        </div>
+        <button type="submit" class="btn">Signup</button>
     </form>
   </div>
 </body>
 </html>
-
